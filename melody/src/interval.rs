1b713a02187b59051173641040a2445bc82ff855
Pozidriv <youri.tam@gmail.com>
2016-05-27 18:34:48 -0400

    Fixed bugs, added dependencies

--- melody/src/interval.rs
+++ melody/src/interval.rs
@@ -1,5 +1,11 @@
 // Object representing an interval
 
+extern crate num;
+
+use std::ops::Mul;
+use self::num::rational::Ratio;
+use self::num::integer::Integer;
+
 #[derive(Copy, Clone)]
 pub struct Interval {
     pitch: f64, // for now, calculated in octaves
@@ -16,25 +22,29 @@ impl Interval {
     }
 }
 
-enum IntervalRepresentation {
+#[derive(Copy, Clone)]
+pub enum IntervalRepresentation {
     Ratio(RatioPower<i32>),
     Cents(i32),
 }
 
 // RationalPower
 
-use num::rational;
-
-pub struct RatioPower<T> {
+#[derive(Copy, Clone)]
+pub struct RatioPower<T> 
+    where T: Clone + Integer 
+{
     exponent: Ratio<T>,
     base: Ratio<T>,
 }
 
-impl Mul<RatioPower<T>> for RatioPower<T> {
-    type Output = RatioPower;
+impl<T> Mul<RatioPower<T>> for RatioPower<T>
+    where T: Clone + Integer
+{
+    type Output = RatioPower<T>;
 
-    pub fn mul(self, other: RatioPower<T>) -> RatioPower<T> {
-        RatioPower<T> { exponent: self.exponent + other.exponent,
+    fn mul(self, other: RatioPower<T>) -> RatioPower<T> {
+        RatioPower::<T> { exponent: self.exponent + other.exponent,
                         base: self.base * other.base, }
     }
 }
